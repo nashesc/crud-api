03e3df116889ce66b127effaba9f0805a6144932
Jonas Escanilla <jonasm.escanilla@gmail.com>
2026-07-16 12:07:38 +0800

    Stage 3: create with validation

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 let tasks = [
    { id: 1, title: "Backend Track Assignment 1 (Morning)", done: true },
    { id: 2, title: "Work Tasks (Afternoon)", done: true },
    { id: 3, title: "Dance Training (Night)", done: false }
 ];
-
-const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
    res.json({
@@ -30,10 +32,27 @@ app.get('/tasks/:id', (req, res) => {
    const task = tasks.find(task => task.id === id)
 
    if (!task) {
-      return res.status(404).json({ error: `Task with id ${id} not found` })
+      return res.status(404).json({ error: `Task ${id} not found` })
    }
 
    res.json(task);
+});
+
+app.post('/tasks', (req, res) => {
+   const { title } = req.body;
+
+   if (!title || title.trim() === '') {
+      return res.status(400).json({ error: "Title is required" });
+   }
+
+   const newTask = {
+      id: tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1, 
+      title,
+      done: false
+   }
+   
+   tasks.push(newTask)
+   res.status(201).json(newTask)
 });
 
 app.listen(PORT, () => {
